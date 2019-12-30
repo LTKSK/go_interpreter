6b710b063d3f568d924787e179b02a704c1246ff
LTKSK <ltksk0714@gmail.com>
2019-12-30 14:35:02 +0900

    return if else true falseのtokenに対応

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -27,6 +27,11 @@ const (
 
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
 )
 
 type Token struct {
@@ -35,12 +40,18 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
 	// user定義のものがあったらIDENTを、fnやletならそれらを返す
+	// lexerでletter(文)だと判定された時はこれを採用する
 	if tok, ok := keywords[ident]; ok {
 		return tok
 	}
