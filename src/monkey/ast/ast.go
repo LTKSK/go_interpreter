ad7876610740017229d90e442d39eb06382437f0
LTKSK <ltksk0714@gmail.com>
2019-12-31 19:06:22 +0900

    各StatementにString関数を実装

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey/token"
+import (
+	"bytes"
+	"monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -20,10 +24,29 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
 }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 // let <ident> = <expr>;
 type LetStatement struct {
@@ -32,19 +55,19 @@ type LetStatement struct {
 	Value Expression  // let x = 5 の 5
 }
 
-func (ls *LetStatement) statementNode() {}
-
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
-
-func (i *Identifier) expressionNode() {}
-
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
+func (ls *LetStatement) String() string {
+	var out bytes.Bufferr
+	out.WriteString(ls.TokenLiteral() + " ") //let
+	out.WriteString(ls.Name.String())        // 変数名
+	out.WriteString("=")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String()) // = の右辺
 	}
-	return ""
+
+	out.WriteString(";")
+	return out.String()
 }
 
 // return <expr>;
@@ -53,6 +76,28 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode() {}
-
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token //the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (rs *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
