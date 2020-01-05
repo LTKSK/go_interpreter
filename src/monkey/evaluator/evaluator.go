9da3f3bc65c6af732f9363d1640415d71afc1233
LTKSK <ltksk0714@gmail.com>
2020-01-05 21:03:58 +0900

    単純な数字だけの文字列をevalにわたす処理を追加

---
+++ src/monkey/evaluator/evaluator.go
@@ -0,0 +1,28 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	// expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
