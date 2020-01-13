ce1526d32a8ddf27f47fdb9471b0d7778fea7a9a
LTKSK <ltksk0714@gmail.com>
2020-01-13 16:31:27 +0900

    builtin関数のlenを実装

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -208,11 +208,13 @@ func evalIdentifier(
 	env *object.Environment,
 ) object.Object {
 	// let statementで定義した内容が取得できなかったならエラー
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(
@@ -249,13 +251,17 @@ func unwrapReturnValue(obj object.Object) object.Object {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		// object.ReturnValueを通らないのでunwrapは不要
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func Eval(node ast.Node, env *object.Environment) object.Object {
