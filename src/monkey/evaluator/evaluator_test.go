dc2f4aa57cb1801e36e6278b0c96b5acedfcdd35
LTKSK <ltksk0714@gmail.com>
2020-01-17 00:20:55 +0900

    objectにハッシュの設定追加。HashLiteralのテスト追加

--- src/monkey/evaluator/evaluator_test.go
+++ src/monkey/evaluator/evaluator_test.go
@@ -399,3 +399,32 @@ func TestArrayIndexExpression(t *testing.T) {
 		}
 	}
 }
+
+func TestHashLiterals(t *tesing.T) {
+	input := `let two = "two";
+	{
+		"one": 10 - 9,
+		two: 1 + 1,
+		"thr" + "ee": 6 / 2,
+		4: 4,
+		true: 5,
+		false: 6,
+	}`
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Hash)
+	if !ok {
+		t.Fatalf("Eval didn't return hash. got=%T(%+v)", evaluated, evaluated)
+	}
+	expected := map[object.HashKey]int64{
+		(&object.String{Value: "one"}).HashKey():   1,
+		(&object.String{Value: "two"}).HashKey():   2,
+		(&object.String{Value: "three"}).HashKey(): 3,
+		(&object.Integer{Value: 4}).HashKey():      4,
+		TRUE.HashKey():                             5,
+		FALSE.HashKey():                            6,
+	}
+	if len(result.Pairs) != len(expected) {
+		t.Fatalf("Hash has wrond num of pairs. got=%d", len(result.Pairs))
+	}
+	testIntegerObject(t, pair.Value, expectedValue)
+}
