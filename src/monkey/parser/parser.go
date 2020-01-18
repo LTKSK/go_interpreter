6c7ad7ff51e74cb64e404b7eee6def301779a125
LTKSK <ltksk0714@gmail.com>
2020-01-18 10:40:48 +0900

    putsのbuiltin関数を追加

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -261,25 +261,6 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	return expression
 }
 
-// func (p *Parser) parseCallArguments() []ast.Expression {
-// 	args := []ast.Expression{}
-// 	if p.peekTokenIs(token.RPAREN) {
-// 		p.nextToken()
-// 		return args
-// 	}
-// 	p.nextToken()
-// 	args = append(args, p.parseExpression(LOWEST))
-// 	for p.peekTokenIs(token.COMMA) {
-// 		p.nextToken()
-// 		p.nextToken()
-// 		args = append(args, p.parseExpression(LOWEST))
-// 	}
-// 	if !p.expectPeek(token.RPAREN) {
-// 		return nil
-// 	}
-// 	return args
-// }
-
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	exp := &ast.CallExpression{Token: p.curToken, Function: function}
 	exp.Arguments = p.parseExpressionList(token.RPAREN)
